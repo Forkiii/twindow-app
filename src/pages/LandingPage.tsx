12dae66f0ee8250d428171c33b91bbc015b27c2e
Forkiii <84695575+Forkiii@users.noreply.github.com>
2025-08-06 04:13:57 +0400

    css work

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,7 +3,9 @@ const LandingPage = () => {
   return (
     <div className="page-container">
       <h1 className="page-title">Landing Page</h1>
-      <p className="page-subtitle">This will be the landing page</p>
+      <div className="text-center text-gray-200">
+      <p>This will be the landing page</p>
+      </div>
     </div>
   )
 }
